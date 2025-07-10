7a1c365f0e54b8264c13f477afbd31d057ecd627
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2025-07-10 00:29:30 -0300

    test: dados mongo atlas

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const { scanServer } = require('./serverScanner');
+const { saveDomainsToMongo} = require('./utils/saveDomains.js');
 require('dotenv').config();
 
 const servidores = [
-  {
-    host: '185.239.210.188',
-    username: 'u244644731',
-    password: 'UA5Hywsn&FU3kN#',
-    port: 65002
-  }
+    {
+        host: '185.239.210.188',
+        username: 'u244644731',
+        password: 'UA5Hywsn&FU3kN#',
+        port: 65002
+    }
 ];
 
-(async () => {
-  for (const server of servidores) {
-    const dominios = await scanServer(server);
-  }
-})();
+async function main() {
+    for (const server of servidores) {
+        // const dominios = await scanServer(server);
+
+        const paths = [
+            "/home/user1/web/site1.com",
+            "/home/user2/web/site2.com"
+        ];
+
+        await saveDomainsToMongo(paths, server.host);
+    }
+};
+
+main();
---
+++ utils/saveDomains.js
@@ -0,0 +1,34 @@
+// utils/saveDomains.js
+import Domain from '../src/models/Domain.js';
+
+export async function saveDomainsToMongo(paths = [], server_ip) {
+  const insertPromises = [];
+
+  for (const path of paths) {
+    const parts = path.split('/');
+    
+    if (parts.length < 5) continue; // proteção contra erros
+
+    const server_user = parts[2]; // /home/user/web/domain
+    const server_domain = parts[4];
+
+    const doc = {
+      server_user,
+      server_domain,
+      server_ip,
+      server_suspended: false,
+      // os outros campos ficarão nulos por padrão
+    };
+
+    insertPromises.push(
+      Domain.updateOne(
+        { server_domain }, // evita duplicidade
+        { $set: doc },
+        { upsert: true }
+      )
+    );
+  }
+
+  await Promise.all(insertPromises);
+  console.log(`${insertPromises.length} domínios inseridos ou atualizados com sucesso.`);
+}
