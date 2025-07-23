4b78d74266295feee4d71069bee49c4955c14f74
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2025-07-23 00:34:02 -0300

    fix: cors autorization

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ import express from 'express';
 import database from '../config/database.js';
 import routes from './routes/index.js';
 import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 database.connectDB();
 
-const app = express();
 routes(app);
 
 const PORT = process.env.PORT || 3000;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import domains from './domainRoutes.js';
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Server Clean Project!"));
-
     app.use(express.json(), domains);
 };
 
